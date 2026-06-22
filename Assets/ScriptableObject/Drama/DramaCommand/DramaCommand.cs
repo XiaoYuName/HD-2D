45aa38307261b25f999ba4f8b10f5406e40052f1
LiZhenYu <2383433922@qq.com>
2026-06-22 16:21:44 +0800

    修复适配问题

--- Assets/ScriptableObject/Drama/DramaCommand/DramaCommand.cs
+++ Assets/ScriptableObject/Drama/DramaCommand/DramaCommand.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using Sirenix.OdinInspector;
-using UnityEditor.Localization;
-using UnityEngine;
 
 
 /// <summary>
