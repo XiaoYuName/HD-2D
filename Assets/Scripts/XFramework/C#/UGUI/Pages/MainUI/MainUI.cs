d36c7753ee88563c03343878c693321c3b623abf
LiZhenYu <2383433922@qq.com>
2026-07-01 17:13:15 +0800

    提交1

--- Assets/Scripts/XFramework/C#/UGUI/Pages/MainUI/MainUI.cs
+++ Assets/Scripts/XFramework/C#/UGUI/Pages/MainUI/MainUI.cs
@@ -158,7 +158,7 @@ public class MainUI : UIBase
         if (!GameSceneManager.Instance.ContainsWordMapScene(sceneData.WordMapSceneID))
         {
             leftButton.interactable = false;
-            sceneNameStringEvent.SetText("WordScene","Scene10001Name");
+            sceneNameStringEvent.SetText("WordScene","Scene10000Name");
             StartCoroutine(OnPreRender());
             rightButton.interactable = false;
         }
