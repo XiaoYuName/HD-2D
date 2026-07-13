51a255c4f5fda7c35dc4222b4086b2007ee57b23
LiZhenYu <2383433922@qq.com>
2026-07-13 09:47:14 +0800

    线上界面

--- Assets/Scripts/XFramework/C#/UGUI/Pages/ClawMachineGuideUI/ClawMachineGuidItemSlot.cs
+++ Assets/Scripts/XFramework/C#/UGUI/Pages/ClawMachineGuideUI/ClawMachineGuidItemSlot.cs
@@ -54,7 +54,6 @@ public class ClawMachineGuidItemSlot : UIBase,IPointerClickHandler
 
     public void UpdateData(ItemInfo itemInfo)
     {
-        Debug.LogError("刷新解锁状态~~");
         if (itemInfo != null)
         {
             if (!InventoryManager.Instance.HasItemUnlock(itemInfo.ID))
