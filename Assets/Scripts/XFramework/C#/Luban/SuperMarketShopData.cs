1d5c6d20432396c282636351aac829043e87b294
Junjiuyue <2298876426@qq.com>
2026-07-14 09:26:10 +0800

    商店表修正
    商店表修正

--- Assets/Scripts/XFramework/C#/Luban/SuperMarketShopData.cs
+++ Assets/Scripts/XFramework/C#/Luban/SuperMarketShopData.cs
@@ -25,6 +25,7 @@ public sealed partial class SuperMarketShopData : Luban.BeanBase
         Price = (int)_obj.GetValue("Price");
         UpdateMode = (ShopUpdateType)(int)_obj.GetValue("UpdateMode");
         BuyLimit = (int)_obj.GetValue("BuyLimit");
+        ShopHelpIconName = (string)_obj.GetValue("ShopHelpIconName");
     }
 
     public static SuperMarketShopData DeserializeSuperMarketShopData(JToken _buf)
@@ -52,6 +53,10 @@ public sealed partial class SuperMarketShopData : Luban.BeanBase
     /// 购买上限
     /// </summary>
     public readonly int BuyLimit;
+    /// <summary>
+    /// 商店帮忙图标名
+    /// </summary>
+    public readonly string ShopHelpIconName;
 
 
     public const int __ID__ = 1220845655;
@@ -69,6 +74,7 @@ public sealed partial class SuperMarketShopData : Luban.BeanBase
         + "Price:" + Price + ","
         + "UpdateMode:" + UpdateMode + ","
         + "BuyLimit:" + BuyLimit + ","
+        + "ShopHelpIconName:" + ShopHelpIconName + ","
         + "}";
     }
 }
