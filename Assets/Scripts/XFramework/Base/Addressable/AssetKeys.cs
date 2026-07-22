75f7bd3f0dffab947f90155c543e9f88cf7af516
DaiZhen <571876754@qq.com>
2026-07-22 11:08:35 +0800

    修改Root场景管理器，修复立绘图bug

--- Assets/Scripts/XFramework/Base/Addressable/AssetKeys.cs
+++ Assets/Scripts/XFramework/Base/Addressable/AssetKeys.cs
@@ -591,6 +591,7 @@ namespace XFramework
         public const string 果蔬店大叔2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/DialogueTexture2D/果蔬店大叔@2x.png";
         public const string 酒保2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/DialogueTexture2D/酒保@2x.png";
         public const string MachiPath3 = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/Machi.png";
+        public const string OldFishermanPath2 = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/OldFisherman.png";
         public const string ShangDianXiaoMeiPath2 = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/ShangDianXiaoMei.png";
         public const string 单人女二2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/单人_女二@2x.png";
         public const string 单人女仆2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/单人_女仆@2x.png";
@@ -598,7 +599,6 @@ namespace XFramework
         public const string 单人布料店2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/单人_布料店@2x.png";
         public const string 单人摄影小妹2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/单人_摄影小妹@2x.png";
         public const string 单人果蔬大叔2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/单人_果蔬大叔@2x.png";
-        public const string 单人渔夫2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/单人_渔夫@2x.png";
         public const string 单人经理2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/单人_经理@2x.png";
         public const string 单人酒保2xPath = "Assets/AddressableAssets/Remote/Texture2D/Characters/SceneTexture2D/单人_酒保@2x.png";
         public const string Cloting0Path = "Assets/AddressableAssets/Remote/Texture2D/Clothing/Cloting_0.png";
